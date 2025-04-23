141ca68ae35e465b4677ce5e2ac3f80fc060e3a9
Sobrukai404 <lucas.lima4138@gmail.com>
2025-04-23 20:05:11 -0300

    ajustado o app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userRoutes from "./controllers/userController.js";
+import functionRoutes from "./controllers/functionController.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/users", userRoutes);
+app.use("/functions", functionRoutes);
 
 export default app;
